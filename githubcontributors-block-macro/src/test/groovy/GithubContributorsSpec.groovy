877dc9ca44eeba26cfe1a0e94c8fdad952e94f55
Robert.Panzer <robert.panzer@me.com>
2015-07-07 09:33:02 +0200

    Set safe mode in test to have css in resulting html file

--- githubcontributors-block-macro/src/test/groovy/GithubContributorsSpec.groovy
+++ githubcontributors-block-macro/src/test/groovy/GithubContributorsSpec.groovy
@@ -3,6 +3,7 @@ package org.asciidoctor.extensionslab.githubcontributors
 import groovy.util.logging.Log
 import org.asciidoctor.Asciidoctor
 import org.asciidoctor.OptionsBuilder
+import org.asciidoctor.SafeMode
 import spock.lang.Specification
 
 @Log
@@ -14,7 +15,9 @@ class GithubContributorsSpec extends Specification {
         Asciidoctor asciidoctor = Asciidoctor.Factory.create()
 
         when:
-        log.info asciidoctor.convertFile(new File('src/test/resources/asciidoctorj.adoc'), OptionsBuilder.options().headerFooter(true))
+        log.info asciidoctor.convertFile(
+                new File('src/test/resources/asciidoctorj.adoc'),
+                OptionsBuilder.options().headerFooter(true).safe(SafeMode.UNSAFE))
 
         then:
         noExceptionThrown()
